e775ba8665b5cf1040cf2945b836c311b39d5db9
Hector Mansilla Arias <hector.mansilla@ibm.com>
2021-03-05 17:58:21 -0300

    Using CSS components

--- src/PeopleListItem.js
+++ src/PeopleListItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './PeopleListItem.css';
 
 export const PeopleListItem = ({ person }) => (
-    <div>
+    <div className="list-item-container">
         <h3>{person.name}</h3>
         <p>Age: {person.age}</p>
         <p>Hair Color: {person.hairColor}</p>
