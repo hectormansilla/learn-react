79fa9b1d563abf373eb1bfa1ab72668d57297778
Hector Mansilla Arias <hector.mansilla@ibm.com>
2021-03-05 12:26:01 -0300

    my first component

---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Greeting = () => {
+    let isMorning =  (new Date()).getHours() < 12;
+    let greetingHeader = isMorning
+      ? <h3>Good Morning!</h3>
+      : <h3>Good Evening!</h3>
+    
+    return (
+        <>
+        {greetingHeader}
+        <p>You hace 5 new messages</p>
+        </>
+    )
+}
