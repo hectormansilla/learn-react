9f147cf7c0956bde5dfd35b27d4fdf8a9aef57c4
Hector Mansilla Arias <hector.mansilla@ibm.com>
2021-03-08 17:19:17 -0300

    Some fixes to fetch data from API, now with axios and user avatar

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const UserProfilePage = () => {
     const [user,setUser] = useState({ name: {}});
-
-    useEffect(async () =>{
+    const [avatar, setAvatar] = useState();
+    useEffect(() =>{
         const fetchUser = async () => {
-            const response = await fetch('https://randomuser.me/api');
-            const data = await response.json();
-            setUser(data.results[0]);
+            const response = await axios.get('https://randomuser.me/api');
+            setUser(response.data.results[0]);
+            console.log(response.data.results[0].picture.medium);
+            setAvatar(response.data.results[0].picture.medium);
         }
 
         fetchUser();
+        
     }, []);
 
     return (
         <div>
-            <h3>Name: {user.name.fisrt} {user.name.last}</h3>
+            <img src={avatar} alt='User' />
+            <h3>Name: {user.name.first} {user.name.last}</h3>
             <p>Email: {user.email}</p>
         </div>
     );
